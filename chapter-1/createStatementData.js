61f18605f27f90983abe3dbf542071ffe07f3e49
Candido Sales Gomes <candidosg@gmail.com>
2023-12-03 22:04:33 -0500

    pag-44

--- chapter-1/createStatementData.js
+++ chapter-1/createStatementData.js
@@ -1,3 +1,68 @@
+class PerformanceCalculator {
+  constructor(aPerformance, aPlay) {
+    this.performance = aPerformance;
+    this.play = aPlay;
+  }
+
+  /*
+   * Step 1 - Extract function
+   * The code will tell me what it's doing. I don't have to figure it out again.
+   * Step 16 - Move function
+   */
+  get amount() {
+    throw new Error('subclass responsability');
+  }
+
+  /*
+   * Step 17 - Move function
+   */
+  get volumeCredits() {
+    return Math.max(this.performance.audience - 30, 0);
+  }
+}
+
+/*
+ * Step 18 - Replace Conditional with Polymorphism
+ */
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+    return result;
+  }
+}
+
+/*
+ * Step 18 - Replace Conditional with Polymorphism
+ */
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+    return result;
+  }
+
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
+  }
+}
+
+function createPerformanceCalculator(aPerformance, aPlay) {
+  switch (aPlay.type) {
+    case 'tragedy':
+      return new TragedyCalculator(aPerformance, aPlay);
+    case 'comedy':
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`unknow type: ${aPlay.type}`);
+  }
+}
+
 export default function createStatementData(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
@@ -11,11 +76,18 @@ export default function createStatementData(invoice, plays) {
    * as much as I can - mutable state quickly becomes something rotten.
    */
   function enrichPerformance(aPerformance) {
+    /*
+     * Step 18 - Replace Constructor with Factory Function
+     */
+    const calculator = createPerformanceCalculator(
+      aPerformance,
+      playFor(aPerformance),
+    );
     const result = Object.assign({}, aPerformance);
     // Step 14 - Move Function
-    result.play = playFor(result);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
+    result.play = calculator.play;
+    result.amount = calculator.amount;
+    result.volumeCredits = calculator.volumeCredits;
     return result;
   }
 
@@ -24,46 +96,6 @@ export default function createStatementData(invoice, plays) {
    */
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
-  }
-
-  /*
-   * Step 1 - Extract function
-   * The code will tell me what it's doing. I don't have to figure it out again.
-   */
-  function amountFor(aPerformance) {
-    let result = 0;
-    switch (aPerformance.play.type) {
-      case 'tragedy':
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case 'comedy':
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`unknow type: ${aPerformance.play.type}`);
-    }
-    return result;
-  }
-
-  /*
-   * Step 5 - Extract function
-   */
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    // add volume credits
-    result += Math.max(aPerformance.audience - 30, 0);
-    // add extra credit for every ten comedy attendees
-    if ('comedy' === aPerformance.play.type)
-      result += Math.floor(aPerformance.audience / 5);
-
-    return result;
   }
 
   /*
